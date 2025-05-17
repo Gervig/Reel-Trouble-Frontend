76ab13b28ce099ffa3ba99b6dd4fdb4e489e301a
Gervig <gervig91@gmail.com>
2025-05-17 18:14:34 +0200

    Create Vision.jsx

---
+++ app/reel-trouble/src/pages/Vision.jsx
@@ -0,0 +1,24 @@
+function Vision() {
+  return (
+    <div>
+      <h2>Vision of the API</h2>
+      <p>
+        ReelTrouble is An API that suggests movies based on user preferences.
+      </p>
+      <p>
+        A user can like movies, and therefore make sure the movies that are
+        suggested, aren't movies they have watched before.
+      </p>
+      <p>
+        A user can choose to get a movie suggestion within a specific genre or
+        randomly.
+      </p>
+      <p>
+        Anyone can choose to get a movie suggestion within a specific genre, but
+        here their like list is not included.
+      </p>
+    </div>
+  );
+}
+
+export default Vision;
