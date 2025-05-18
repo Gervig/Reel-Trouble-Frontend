d4ecf4fa44577bcb42ddfc9b4fa64b907767a636
Gervig <gervig91@gmail.com>
2025-05-18 23:04:36 +0200

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,7 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import LoginForm from "../components/LoginForm";
-import facade from "../util/apiFacade";
+// import facade from "../util/apiFacade";
 import styles from "../App.module.css";
-
 
 function Home() {
   return (
