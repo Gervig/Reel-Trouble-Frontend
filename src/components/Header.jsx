4a0cf9b3caae548083862591458b1180ce31b6f4
Gervig <gervig91@gmail.com>
2025-05-18 21:42:43 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
 import { Link } from "react-router-dom";
 import styles from "../App.module.css";
+import { useState } from "react";
 
 function Header() {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const staticSrc =
+    "https://raw.githubusercontent.com/Gervig/ReelTrouble/main/docs/reel_trouble.png";
+  const gifSrc =
+    "https://raw.githubusercontent.com/Gervig/images-for-hosting/refs/heads/main/reeltrouble2.gif";
+
   return (
-    <Link to="/" className={styles.header} style={{ display: "block", textDecoration: "none" }}>
+    <Link
+      to="/"
+      className={styles.header}
+      style={{ display: "block", textDecoration: "none" }}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <div>
         <img
           src="https://raw.githubusercontent.com/Gervig/ReelTrouble/refs/heads/main/docs/reel_trouble.png"
