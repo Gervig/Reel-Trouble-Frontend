4572a5f83a78ea20d1ab7cae460b806ec37a8714
Gervig <gervig91@gmail.com>
2025-05-22 23:39:01 +0200

    spinner css

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,9 +1,10 @@
 import loadingGif from "../assets/rt_loader.gif";
+import styles from "../App.module.css";
 
 function Spinner() {
   return (
-    <div style={{ textAlign: "center", margin: "2rem" }}>
-      <img src={loadingGif} alt="Loading..." width="100" />
+    <div className={styles.spinner} >
+      <img  src={loadingGif} alt="Loading..." width="100" />
     </div>
   );
 }
